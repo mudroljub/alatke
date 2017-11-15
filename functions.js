c98a039aa6cdca5d6df5425bbbf95966cd0f90dd
Damjan Pavlica <mudroljub@gmail.com>
2017-11-15 17:29:09 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,3 +1,6 @@
+// uklanja duplikate iz niza objekata
+izreke = izreke.filter((obj, i, self) => self.findIndex((t) => t.text === obj.text) === i)
+
 export function isValidEmail(email) {
   var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return regex.test(email);
@@ -12,5 +15,13 @@ export function loadData(url, calback) {
   http.send(null);
 }
 
-// uklanja duplikate iz niza objekata
-izreke = izreke.filter((obj, i, self) => self.findIndex((t) => t.text === obj.text) === i)
+function ucitaj(slike, povratna) {
+  let ucitaneSlike = 0
+  slike.map(src => {
+    const slika = new Image()
+    slika.onload = () => {
+      if (++ucitaneSlike == slike.length) povratna()
+    }
+    slika.src = src
+  })
+}
